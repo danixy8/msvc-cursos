f3730bada5b49f080c7bb7337dc9cd3e7794e97a
danixy8 <danielguve2@gmail.com>
2022-06-26 23:27:44 -0400

    implementacion de openfeign y comunicacion entre microservicios usuarios y cursos

--- src/main/java/org/danixy8/springcloud/msvc/cursos/repositories/CursoRepository.java
+++ src/main/java/org/danixy8/springcloud/msvc/cursos/repositories/CursoRepository.java
@@ -1,6 +1,6 @@
 package org.danixy8.springcloud.msvc.cursos.repositories;
 
-import org.danixy8.springcloud.msvc.cursos.entity.Curso;
+import org.danixy8.springcloud.msvc.cursos.models.entity.Curso;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CursoRepository extends CrudRepository<Curso, Long> {
--- src/main/java/org/danixy8/springcloud/msvc/cursos/services/CursoService.java
+++ src/main/java/org/danixy8/springcloud/msvc/cursos/services/CursoService.java
@@ -1,6 +1,7 @@
 package org.danixy8.springcloud.msvc.cursos.services;
 
-import org.danixy8.springcloud.msvc.cursos.entity.Curso;
+import org.danixy8.springcloud.msvc.cursos.models.Usuario;
+import org.danixy8.springcloud.msvc.cursos.models.entity.Curso;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,11 @@ import java.util.Optional;
 public interface CursoService {
     List<Curso> listar();
     Optional<Curso> porId(Long id);
+    Optional<Curso> porIdConUsuarios(Long id);
     Curso guardar(Curso curso);
     void eliminar(Long id);
+
+    Optional<Usuario> asignarUsuario(Usuario usuario, Long cursoId);
+    Optional<Usuario> crearUsuario(Usuario usuario, Long cursoId);
+    Optional<Usuario> eliminarUsuario(Usuario usuario, Long cursoId);
 }
